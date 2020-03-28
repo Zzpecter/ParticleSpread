4f379a23f7918b9a21a71b953686ea1dd348c410
René Vilar Schlichter <renevs91@gmail.com>
2020-03-28 08:11:18 -0400

    first update
    particles now are shot correctly from a host.

--- ps.py
+++ ps.py
@@ -4,66 +4,89 @@ import random
 import numpy as np
 
 SCREEN_SIZE = WIDTH, HEIGHT = (640, 480)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 50, 50)
-GREEN = (50, 255, 50)
-personRadius = 30
-virusRadius = 3
 
 # Initialization
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption('Circles')
 fps = pygame.time.Clock()
-paused = False
-spread = False
 
 
-xFactors = random.sample(range(1, 11), 10)
-yFactors = random.sample(range(-10, 10), 10)
-
-virusBalls = []
-# Ball setup
-personPos = [100, 240]
-virusStartPos = [100 + personRadius, 240]
+def update(renderList):
+    for objct in renderList:
+        if(objct.isVirus()):
+            objct.move()
 
 
-def update():
-    #list of x and y factors 
-    for i in range(0,9):
-        print(virusBalls[i][0])
-        print(xFactors[i])
-        virusBalls[i][0] += xFactors[i]
-        virusBalls[i][1] += yFactors[i]
-
-
-def render():
+def render(renderList):
     screen.fill(BLACK)
-    pygame.draw.circle(screen, WHITE, personPos, personRadius, 0)
 
-    if spread:
-        for i in range(0,9):
-            pygame.draw.circle(screen, RED, virusBalls[i], virusRadius, 0)
+    for objct in renderList:
+        print(objct.pos)
+        print(objct.speed)
+        pygame.draw.circle(screen, objct.color, objct.pos, objct.radius, 0)
 
     pygame.display.update()
-    fps.tick(6)
-
-def Spread():
-    for i in range(0,10):
-        virusBalls.insert(0,virusStartPos)
+    fps.tick(10)
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                spread = True
-                Spread()
-    if not paused:
-        if spread:
-            update()
-        render()
+class Ball():
+    def __init__(self, color, radius, pos,  speed):
+        self.color = color
+        self.radius = radius
+        self.pos = pos
+        self.speed = speed
+
+    def isPerson(self):
+        return False
+
+    def isVirus(self):
+        return False
+
+    def move(self):
+        self.pos[0] += self.speed[0]
+        self.pos[1] += self.speed[1]
+
+class Person(Ball):
+    def __init__(self, pos, color = (255, 255, 255), radius = 30, speed = [0, 0], immunity=0):
+        self.color = color
+        self.radius = radius
+        self.pos = pos
+        self.speed = speed
+        self.immunity = immunity
+
+    def isPerson(self):
+        return True
+
+class Virus(Ball):
+    def __init__(self, lifeTime, pos, color = (255, 50, 50), radius = 2, speed = [0, 0]):
+        self.color = color
+        self.radius = radius
+        self.pos = pos
+        self.speed = speed
+        self.lifeTime = lifeTime
+        self.speed = [random.randint(1,10), random.randint(-10,10)]
+        self.lifeTime = lifeTime
+
+    def isVirus(self):
+        return True
+
+if __name__ == '__main__':
+
+    testSubject = Person(pos =  [100, 240])
+    renderList = []
+    renderList.append(testSubject)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    numP = random.randint(3,20)
+                    for i in range(numP):
+                        renderList.append(Virus(lifeTime = 10, pos = [130, 240]))
+        
+        update(renderList)
+        render(renderList)
